9b6f3bd3f26ba77c1a679d1a9baecb1583433e31
PJK <PJK@gmail.com>
2023-03-09 15:14:00 +0700

    update

--- JSD#4 GenTH/JS/script.js
+++ JSD#4 GenTH/JS/script.js
@@ -1,6 +1,7 @@
-document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
+// document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
 function Hello() {
   alert("Helllo, World!");
 //   document.getElementsById("text")
+document.getElementById("text").innerHTML = "The button has been clicked";
   console.log("The button han been clicked");
 }
