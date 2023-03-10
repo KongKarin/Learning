7253f376c5a5ff98d89cf5c49941287a303745be
PJK <PJK@gmail.com>
2023-03-11 00:35:51 +0700

    updatee

---
+++ JSD#4 GenTH/JS/miniexjs.js
@@ -0,0 +1,16 @@
+// console.log('Hello world');
+// let email = 'kong@gmail.com';
+// console.log(email);
+
+let firstName = 'Karin';
+let lastName = 'Naprang';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+let info = `My name is ${fullName} and I am ${firstName}`;
+console.log(info);
+
+console.log(fullName.length);
+
+// console.log(fullName.toUpperCase);
