ef1d9bc8ec36b681665775e6fb30f4de9c0052ee
KongKarin <kong.1475953@gmail.com>
2023-03-20 14:51:12 +0700

    learnbuildform

--- JSD4GenTH/HTML/Form.js
+++ JSD4GenTH/HTML/Form.js
@@ -1,3 +1,30 @@
-const document.GetElementByid = function (fname) {
-    
+const getValue = () => {
+    let firstName = document.getElementById("fname").value;
+    let lastName = document.getElementById("lname").value;
+    let password = document.getElementById("password").value;
+    let email = document.getElementById("email").value;
+
+    if (!firstName) {
+
+    }
+}
+
+if (password) {
+    console.log("No password);
+}
+
+
+// P'Lak Code
+
+form.addEventListener("submit, (e) => {
+    let message = [];
+    if(fnam.value === '' || fname.value === null){
+        message.push('Name is required')
+    }
+
+    if(message.length > 0){
+    e.preventDefault();
+    errorElement.innerHTML = message.join(',');
+    }
+    console.log(fname);
 }
