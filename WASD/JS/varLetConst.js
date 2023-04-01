0aa920593f14d7037cdbd23ad84a7e01ea783d4e
KongKarin <kong.1475953@gmail.com>
2023-04-02 05:48:02 +0700

    createtest

---
+++ WASD/JS/varLetConst.js
@@ -0,0 +1,22 @@
+let name = "John";
+let lastname = "Doe";
+
+function greeting() {
+    return 'Hello, ' + name + ' ' + lastname;
+}
+
+greeting();
+
+console.log(greeting());
+
+// Template literals, Template Strings
+function greeting() {
+    return 'Hello, ' + name + ' ' + lastname;
+}
+
+greeting();
+
+console.log(greeting());
+
+
+
