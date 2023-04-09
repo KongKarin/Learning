3620cce99aecc2bdf28de4e8349b101f7f9ba906
KongKarin <kong.1475953@gmail.com>
2023-04-10 06:34:38 +0700

    1111

---
+++ dailyQuestion/10_04_23.js
@@ -0,0 +1,4 @@
+// 1.Assume you are going to the supermarket, and you pick many items and put to the basket
+//  -Write a program that calculates the total cost of items in a shopping cart, including tax.
+//  -From the above question, separate total cost by category
+// 2.Write a program that calculates the amount of interest earned on a bank account given the principal, interest rate, and time.
