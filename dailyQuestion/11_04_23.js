443e41d94f06375746d9c1e3dd8a0e23521d47b6
KongKarin <kong.1475953@gmail.com>
2023-04-11 07:54:29 +0700

    upload

---
+++ dailyQuestion/11_04_23.js
@@ -0,0 +1,11 @@
+// Daily Question - 11/04/2023
+// 1.In a football league, matches are not only won by teams but players who excel on the field can also be recognized as the Most Valuable Player (MVP) of the match. Given a list of matches and their scores
+    // - Write a program that determines the winner of the league
+    // - Find the player who has been awarded the most MVP titles throughout the season.
+// Example format
+    // "matches": [
+    // {"homeTeam": "Arsenal", "awayTeam": "Chelsea", "homeScore": 2, "awayScore": 1, "MVP": "Person A"},
+    // {"homeTeam": "Liverpool", "awayTeam": "Manchester City", "homeScore": 1, "awayScore": 1, "MVP": "Person B"},
+    // {"homeTeam": "Manchester United", "awayTeam": "Tottenham Hotspur", "homeScore": 3, "awayScore": 0, "MVP": "Person C"}
+    // ]
+// 2.Thinking about some sport/competition you like, Design scenarios and solve 1 problem by your own
